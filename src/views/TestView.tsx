074522b99dab9133c339e445e2506624da8a7590
Jelle van Snik <jellevs@gmail.com>
2022-12-24 01:35:41 +0100

    working P2P

--- src/views/TestView.tsx
+++ src/views/TestView.tsx
@@ -7,6 +7,7 @@ class PeerConnection {
   _peerConn: RTCPeerConnection | null = null;
   _channel: RTCDataChannel | null = null;
   _msgCb: ((txt: string) => void) | null = null;
+  _iceLastCb: ((local: string) => void) | null = null;
 
   _initDataChannel() {
     if (this._channel) {
@@ -40,6 +41,8 @@ class PeerConnection {
         return;
       }
       console.log("ICE candidate received (LAST)", peerConn.localDescription);
+      if (this._iceLastCb)
+        this._iceLastCb(JSON.stringify(peerConn.localDescription));
     };
     peerConn.onconnectionstatechange = (evt) => {
       console.log("PEER connection state changed", evt);
@@ -55,32 +58,36 @@ class PeerConnection {
     this._peerConn = peerConn;
   }
 
-  async createOffer(): Promise<string> {
+  async createOffer() {
     this._createConnection();
     this._channel = this._peerConn?.createDataChannel("test") ?? null;
     this._initDataChannel();
     if (!this._peerConn) throw new Error("failed create peer conn");
     const offer = await this._peerConn.createOffer();
     await this._peerConn.setLocalDescription(offer);
-    return JSON.stringify(offer);
   }
 
   async respondToAnswer(answer: string) {
     if (!this._peerConn) throw new Error("failed create peer conn");
-    await this._peerConn.setRemoteDescription(JSON.parse(answer));
+    const desc = new RTCSessionDescription(JSON.parse(answer));
+    await this._peerConn.setRemoteDescription(desc);
   }
 
-  async connectToOffer(offer: string): Promise<string> {
+  async connectToOffer(offer: string) {
     this._createConnection();
     if (!this._peerConn) throw new Error("failed create peer conn");
-    await this._peerConn.setRemoteDescription(JSON.parse(offer));
+    var desc = new RTCSessionDescription(JSON.parse(offer));
+    await this._peerConn.setRemoteDescription(desc);
     const answer = await this._peerConn.createAnswer();
     await this._peerConn.setLocalDescription(answer);
-    return JSON.stringify(answer);
   }
 
   onMessage(handler: (txt: string) => void) {
     this._msgCb = handler;
+  }
+
+  onLastIce(handler: (txt: string) => void) {
+    this._iceLastCb = handler;
   }
 
   sendMessage(txt: string) {
@@ -93,14 +100,17 @@ function HostView() {
   const [answer, setAnswer] = useState("");
   const [logs, setLogs] = useState<string[]>([]);
   const conn = useRef(new PeerConnection());
+  const [msg, setMsg] = useState("");
 
   useEffect(() => {
     (async () => {
       conn.current.onMessage((txt) => {
         setLogs((arr) => [...arr, txt]);
       });
-      const txtOffer = await conn.current.createOffer();
-      setOffer(txtOffer);
+      conn.current.onLastIce((localOffer) => {
+        setOffer(localOffer);
+      });
+      await conn.current.createOffer();
     })();
   }, []);
 
@@ -108,14 +118,22 @@ function HostView() {
     conn.current.respondToAnswer(answerText);
   }
 
+  async function sendMsg() {
+    conn.current.sendMessage(msg);
+    setLogs((arr) => [...arr, msg]);
+  }
+
   return (
     <div className="flex flex-col">
       <h1>Host</h1>
       <textarea value={offer} />
-      <hr />
+      <hr className="my-4" />
       <textarea onChange={(e) => setAnswer(e.target.value)} value={answer} />
       <button onClick={() => respondAnswer(answer)}>Respond Answer</button>
-      <hr />
+      <hr className="my-4" />
+      <input type="text" onChange={(e) => setMsg(e.target.value)} value={msg} />
+      <button onClick={() => sendMsg()}>Send message</button>
+      <hr className="my-4" />
       {logs.map((v) => (
         <p>{v}</p>
       ))}
@@ -128,26 +146,43 @@ function ClientView() {
   const [answer, setAnswer] = useState("");
   const [msg, setMsg] = useState("");
   const conn = useRef(new PeerConnection());
+  const [logs, setLogs] = useState<string[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      conn.current.onMessage((txt) => {
+        setLogs((arr) => [...arr, txt]);
+      });
+      conn.current.onLastIce((localAnswer) => {
+        setAnswer(localAnswer);
+      });
+      await conn.current.createOffer();
+    })();
+  }, []);
 
   async function connect(offerText: string) {
-    const answerText = await conn.current.connectToOffer(offerText);
-    setAnswer(answerText);
+    await conn.current.connectToOffer(offerText);
   }
 
   async function sendMsg() {
     conn.current.sendMessage(msg);
+    setLogs((arr) => [...arr, msg]);
   }
 
   return (
     <div className="flex flex-col">
       <h1>client</h1>
       <textarea value={answer} />
-      <hr />
+      <hr className="my-4" />
       <textarea onChange={(e) => setOffer(e.target.value)} value={offer} />
       <button onClick={() => connect(offer)}>Connect</button>
-      <hr />
+      <hr className="my-4" />
       <input type="text" onChange={(e) => setMsg(e.target.value)} value={msg} />
       <button onClick={() => sendMsg()}>Send message</button>
+      <hr className="my-4" />
+      {logs.map((v) => (
+        <p>{v}</p>
+      ))}
     </div>
   );
 }
